e46c7688a88f5394d8efdae4dddf6e2edae67779
miluplay <miluplay1013@outlook.com>
2020-08-05 20:18:51 +0800

    去掉无关代码

--- 20.8.5_ICPC_Yoko_2018/C.cpp
+++ 20.8.5_ICPC_Yoko_2018/C.cpp
@@ -1,15 +1,8 @@
-#include <cmath>
 #include <iostream>
-#include <unordered_set>
 #define ll long long
 using namespace std;
-unordered_set<int> check;
 int main() {
   ll x = 1;
-  while (x * 2 > 0) {
-    x *= 2;
-    check.insert(x);
-  }
   ll n;
   while (cin >> n && n != 0) {
     ll N = 2 * n, a = sqrt(N), X, Y;
