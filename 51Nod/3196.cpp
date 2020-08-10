c2206fdd62f19d0934ddb3bc0f6565cdc54307e5
miluplay <miluplay1013@outlook.com>
2020-08-10 20:08:26 +0800

    ggxn

---
+++ 51Nod/3196.cpp
@@ -0,0 +1,12 @@
+// 题目链接：https://www.51nod.com/Challenge/Problem.html#problemId=3196
+// 一个数的阶乘0的个数势必与包含5的个数有关，同时也要考虑在25之后，包含多少个25
+#include <iostream>
+#define BT ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
+int GetAnswer(int a) { return a == 0 ? 0 : a / 5 + GetAnswer(a / 5); }
+using namespace std;
+int main() {
+  BT int a;
+  cin >> a;
+  cout << GetAnswer(a) << endl;
+  return 0;
+}
---
+++ 51Nod/3198.cpp
@@ -0,0 +1,18 @@
+// 题目链接：https://www.51nod.com/Challenge/Problem.html#problemId=3198
+#include <iostream>
+#define BT ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
+using namespace std;
+struct node {
+  string names;
+  int fraction;
+} arr[10010];
+bool cmp(node a,node b){
+  return a.fraction
+}
+int main() {
+  BT int n;
+  for (int i = 0; i < n; i++) cin >> arr[i].names >> arr[i].fraction;
+  sort(arr, arr + n, arr->fraction);
+
+  return 0;
+}
