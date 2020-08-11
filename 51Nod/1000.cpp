b745ad4f9606f6c214a615ab99f25c696325f31f
miluplay <miluplay1013@outlook.com>
2020-08-11 17:26:57 +0800

    riihggx

--- 51Nod/1000.cpp
+++ 51Nod/1000.cpp
@@ -1,10 +1,14 @@
 // Link:https://www.51nod.com/Challenge/Problem.html#problemId=1000
+// Solved!
 // grammar
 #include <iostream>
+
 #define BT ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
 using namespace std;
+
 int main() {
-  BT int a, b;
+  BT
+  int a, b;
   cin >> a >> b;
   cout << a + b;
   return 0;
