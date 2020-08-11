cd15f3e6cd3a273b5e79503596c2b02bd3c9ad71
miluplay <miluplay1013@outlook.com>
2020-08-11 16:45:03 +0800

    删除中文目录

--- 0语法基础/0hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#define BT ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
-using namespace std;
-int main() {
-  BT cout << "Hello World!" << endl;
-  return 0;
-}
